f46bf6addb3cae7c02fb0de84a2334d6c15b9954
AlejoSendra1 <alejo.sendra12@gmail.com>
2023-06-08 14:48:48 -0300

    se unifica el lector y se crean creadores

---
+++ src/main/java/edu/fiuba/algo3/modelo/Creador/CreadorDeMapa.java
@@ -0,0 +1,52 @@
+package edu.fiuba.algo3.modelo.Creador;
+
+
+import edu.fiuba.algo3.modelo.lectorJSON.Camino;
+import edu.fiuba.algo3.modelo.lectorJSON.Lector;
+import edu.fiuba.algo3.modelo.lectorJSON.Mapa;
+import edu.fiuba.algo3.modelo.miscelanea.Coordenada;
+import edu.fiuba.algo3.modelo.parcelas.Rocosa;
+import edu.fiuba.algo3.modelo.parcelas.Tierra;
+import org.json.simple.JSONArray;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CreadorMapa implements Creador {
+    public Camino camino;
+    public Mapa mapa;
+
+    public CreadorMapa(){
+        mapa = new Mapa();
+        camino = new Camino(mapa);
+    }
+
+    public Object crear(){
+        crearMapa(Lector.leer("ArchivosJson/mapa.json", "Mapa"));
+        return mapa;
+    }
+
+    private void crearMapa(JSONArray jsonArray) {
+        AtomicInteger coordY = new AtomicInteger(1);
+        jsonArray.forEach(listaParcelas -> {
+            coordY.getAndIncrement();
+            AtomicInteger coordX = new AtomicInteger(1);
+            ((JSONArray) listaParcelas).forEach(tipoDeTerreno -> {
+                coordX.getAndIncrement();
+                crearParcela((String) tipoDeTerreno, coordY.intValue(), coordX.intValue());
+            });
+        });
+    }
+
+    private void crearParcela(String tipoDeTerreno, int coordY, int coordX){
+        Coordenada nuevaCoordenada = new Coordenada(coordX, coordY);
+        if (tipoDeTerreno.equals("Rocoso")) {
+            mapa.agregar(new Rocosa(nuevaCoordenada));
+        }
+        if (tipoDeTerreno.equals("Pasarela")) {
+            camino.agregar(nuevaCoordenada);
+        }
+        if (tipoDeTerreno.equals("Tierra")) {
+            mapa.agregar(new Tierra(nuevaCoordenada));
+        }
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/Creador/CreadorEnemigos.java
@@ -0,0 +1,43 @@
+package edu.fiuba.algo3.modelo.Creador;
+
+import edu.fiuba.algo3.modelo.*;
+import edu.fiuba.algo3.modelo.Enemigos.Enemigo;
+import edu.fiuba.algo3.modelo.Enemigos.Hormiga;
+import edu.fiuba.algo3.modelo.lectorJSON.Lector;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CreadorEnemigos implements Creador {
+    Queue<ArrayList<Enemigo>> colaSpawner;
+    public CreadorEnemigos(){
+        colaSpawner = new LinkedList<>();
+    }
+
+    public Object crear() {
+        crearTurnoEnemigos(Lector.leer("ArchivosJson/enemigos.json", "Enemigo"));
+        return colaSpawner;
+    }
+
+    public void crearTurnoEnemigos(JSONArray listaTurnos) {
+        listaTurnos.forEach( turno -> crearTurno( (JSONObject) turno) );
+    }
+
+    private void crearTurno(JSONObject turno) {
+        final int POSICION_HORMIGAS = 0;
+        final int POSICION_ARAÑAS = 1;
+        JSONArray.add(enemigoObject.get(key));
+
+        int cantidadHormigas = ((Long) JSONArray.get(POSICION_HORMIGAS)).intValue();
+        for(int i=0; i<cantidadHormigas;i++) {
+            enemigosTurnoActual.add(new Hormiga(pasarelaInicial));
+        }
+
+        colaSpawner.add(enemigosTurnoActual);
+    }
+}
+
