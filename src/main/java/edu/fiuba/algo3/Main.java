bf60e83130eff34f1ede5894c83dec8f8a0eddf0
AlejoSendra1 <alejo.sendra12@gmail.com>
2023-06-14 23:55:22 -0300

    interface

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,7 +1,18 @@
 package edu.fiuba.algo3;
 
-public class Main {
-    public static void main(String[] args) {
-        App.main(args);
+import edu.fiuba.algo3.modelo.Interface.InicializadorVentana;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        InicializadorVentana.start(stage);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,5 @@ module edu.fiuba.algo3 {
     requires javafx.controls;
     requires json.simple;
     exports edu.fiuba.algo3;
+    exports edu.fiuba.algo3.modelo.Interface;
 }
